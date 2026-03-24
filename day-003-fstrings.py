7e7ac588a643025ce8c3ff10a58e801861244d5b
Michael Angelo Peñalosa <269652551+archangel-sys@users.noreply.github.com>
2026-03-24 14:34:59 +0800

    Day 003: f-strings-2

--- day-003-fstrings.py
+++ day-003-fstrings.py
@@ -5,3 +5,7 @@ house="slytherin"
 print (f"my name is {name}")
 print(f"I use {app} for coding")
 print(f"my house is {house}")
+
+current_year=2026
+target_year=2030
+print (f"years remaining in college: {target_year - current_year}") 

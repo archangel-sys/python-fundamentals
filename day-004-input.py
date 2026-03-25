22c0f83638297fb4bdf63b83dccf3f0d0df6e740
Michael Angelo Peñalosa <269652551+xyvaros@users.noreply.github.com>
2026-03-25 22:50:31 +0800

    Day 004: user input

---
+++ day-004-input.py
@@ -0,0 +1,8 @@
+name=input("enter your name")
+age=input("enter your age")
+current_year=input("enter current year")
+grad_year=input("enter grad year")
+
+print(f"name: {name}")
+print (f"age: {age}")
+print(f"years till grad {int(grad_year)-int(current_year)}")

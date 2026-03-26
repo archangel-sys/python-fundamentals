e64c01ff0fd6907e84637023845ce34c9ef18dbd
Michael Angelo Peñalosa <269652551+xyvaros@users.noreply.github.com>
2026-03-26 15:17:20 +0800

    Day 005: added grade checker

--- day-005-conditionals.py
+++ day-005-conditionals.py
@@ -4,3 +4,10 @@ if age >= 18:
     print ("you are an adult.")
 else:
     print ("you are a minor.")
+
+grade=int(input("enter your grade: "))
+
+if grade >= 75:
+    print ("you passed")
+else:
+    print ("you failed")

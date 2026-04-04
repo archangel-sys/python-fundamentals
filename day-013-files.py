4a7662c67fcf62405b7efd4d4cdf34fe45ef53c9
Michael Angelo Peñalosa <269652551+xyvaros@users.noreply.github.com>
2026-04-04 21:48:38 +0800

    Day 014: file handling

--- day-013-files.py
+++ day-013-files.py
@@ -1,5 +1,13 @@
-print ("Write")
 file = open ("notes.txt", "w")
 file.write ("Day 013 - file handling\n")
 file.write ("This is saved permanently\n")
-file.close ()
+file.close()
+
+file = open ("notes.txt", "r")
+content = file.read()
+file.close()
+print (content)
+
+file = open ("notes.txt", "a")
+file.write("Adding a new line without deleting the rest\n")
+file.close()

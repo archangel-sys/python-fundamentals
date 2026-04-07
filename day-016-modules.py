bd3f72f407d0009349537b86d9b863817ab6d1e8
Michael Angelo Peñalosa <269652551+xyvaros@users.noreply.github.com>
2026-04-07 22:10:47 +0800

    Added Labels

--- day-016-modules.py
+++ day-016-modules.py
@@ -1,16 +1,20 @@
+print ("Import Test")
 import math
 print(math.sqrt(25))
 print(math.pi)
 print(math.pow(2, 10))
 
+print ("From Math Import Test")
 from math import sqrt, pi
 print(sqrt(25))
 print(pi)
 
+print ("Random Import Test")
 import random
 print(random.randint(1, 100))
 print(random.choice(["Korea", "Canada", "China", "Spain", "Japan"]))
 
+print ("From Time Import Test")
 from datetime import datetime
 now=datetime.now()
 print(f"Today {now.year}-{now.month}-{now.day}")

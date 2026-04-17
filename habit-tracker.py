c96a32ce7025fa145993604ee96b9dc16c8cbf89
Michael Angelo Peñalosa <269652551+xyvaros@users.noreply.github.com>
2026-04-17 14:16:38 +0800

    habit tracker project part 3

--- habit-tracker.py
+++ habit-tracker.py
@@ -2,3 +2,12 @@ from datetime import datetime
 
 def log_habit(habit):
     now=datetime.now()
+    date=f"{now.year}-{now.month}-{now.day}"
+    file=open("habits.txt", "a")
+    file.write(f"{date} | {habit}\n")
+    file.close()
+    print (f"Logged: {habit}")
+
+def show_log():
+    try:
+        file=open

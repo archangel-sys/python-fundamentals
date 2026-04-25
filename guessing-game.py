0145474e5277fbe30ffcd4d81e5b1cf767a93c11
Michael Angelo Peñalosa <269652551+xyvaros@users.noreply.github.com>
2026-04-25 22:49:48 +0800

    added save results

--- guessing-game.py
+++ guessing-game.py
@@ -2,4 +2,11 @@ import random
 from datetime import datetime
 
 def save_result(attempts):
-    
+    now=datetime.now()
+    date=f"{now.year}, {now.month}, {now.day}"
+    file=open("game-results.txt", "a")
+    file.write(f"{date} | Solved in {attempts} attempts\n")
+    file.close
+
+target=random.randint(1, 100)
+attemps=0
